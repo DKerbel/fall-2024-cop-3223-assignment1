94795402874e3d5a35d9f37e53ac192bbafcad78
DKerbel <do706962@ucf.edu>
2024-09-03 20:46:50 +0000

    creation of c file

---
+++ fracturing.c
@@ -0,0 +1,11 @@
+//********************************************************
+// fracturing.c
+// Author: Dov Kerbel
+// Date: 9/3/24
+// Class: COP 3223, Professor Parra
+// Purpose: This program 
+// 
+// 
+// Input: 
+// Output: (to the command line) 
+//********************************************************
